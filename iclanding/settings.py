72bf39f68a691860faa4a02ec6a150ac57528d2b
Jorgen Post <jrgnpst@gmail.com>
2023-06-13 18:44:02 +0300

    db to postgres

--- iclanding/settings.py
+++ iclanding/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'iclanding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
