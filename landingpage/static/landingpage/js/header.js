65189a7ea1ca5ff4370e7e0f29f3bb1d65d5287a
Jorgen Post <jrgnpst@gmail.com>
2023-06-23 18:37:14 +0300

    mobile menu js

--- landingpage/static/landingpage/js/header.js
+++ landingpage/static/landingpage/js/header.js
@@ -1,16 +1,18 @@
-console.log('Script loaded!');
-
 document.addEventListener("DOMContentLoaded", function() {
     let menuOpen = false;
     const menuButton = document.querySelector('#menuButton');
+    const closeButton = document.querySelector('#menuButtonClose');
     const mobileMenu = document.querySelector('#mobileMenu');
 
-    menuButton.addEventListener('click', function() {
+    function toggleMenu() {
         menuOpen = !menuOpen;
         if (menuOpen) {
             mobileMenu.classList.remove('hidden');
         } else {
             mobileMenu.classList.add('hidden');
         }
-    });
+    }
+
+    menuButton.addEventListener('click', toggleMenu);
+    closeButton.addEventListener('click', toggleMenu);
 });
