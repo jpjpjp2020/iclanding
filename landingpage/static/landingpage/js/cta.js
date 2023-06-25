627ad1df66f1bf8e0c6a4742c689d48c6a486647
Jorgen Post <jrgnpst@gmail.com>
2023-06-25 17:02:42 +0300

    tw cta

--- landingpage/static/landingpage/js/cta.js
+++ landingpage/static/landingpage/js/cta.js
@@ -5,9 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
   tabs.forEach(tab => {
     tab.addEventListener('click', function() {
       const target = this.dataset.target;
-      tabs.forEach(t => t.classList.remove('active'));
+
+      tabs.forEach(t => {
+        t.classList.remove('active', 'bg-indigo-800', 'text-white');
+        t.classList.add('bg-white', 'text-indigo-800');
+      });
+      this.classList.add('active', 'bg-indigo-800', 'text-white');
+      this.classList.remove('bg-white', 'text-indigo-800');
+
       contents.forEach(c => c.classList.add('hidden'));
-      this.classList.add('active');
       document.querySelector('#' + target + '-content').classList.remove('hidden');
     });
   });
