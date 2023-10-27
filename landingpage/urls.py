00846be40533489640044e7a5039fe05595ca58a
jpjpjp2020 <jrgnpst@gmail.com>
2023-10-27 16:58:54 +0300

    terms and cookies rough

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -6,5 +6,7 @@ app_name = 'landingpage'
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('privacy/', views.privacy_view, name='privacy'),
+    path('terms/', views.terms_view, name='terms'),
+    path('cookies/', views.cookies_view, name='terms'),
     path('sitemap/', views.sitemap_view, name='sitemap'),
 ]
--- landingpage/views.py
+++ landingpage/views.py
@@ -8,6 +8,11 @@ def home_view(request):
 def privacy_view(request):
     return render(request, 'docs/privacy.html')
 
+def terms_view(request):
+    return render(request, 'docs/terms.html')
+
+def cookies_view(request):
+    return render(request, 'docs/cookies.html')
 
 def sitemap_view(request):
     return render(request, 'landingpage/sitemap.xml', content_type='application/xml')
